4755fda28f8fd4f7fafdfd93504bb746732d73c2
cemil yıldız <cmlyldz07@gmail.com>
2020-05-29 02:22:04 +0200

    First commit

---
+++ DataGrid.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Task
+{
+    partial class DataGrid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.selectCsvBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.Size = new System.Drawing.Size(731, 260);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(837, 266);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.selectCsvBtn, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.deleteBtn, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(740, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(94, 260);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // selectCsvBtn
+            // 
+            this.selectCsvBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectCsvBtn.Location = new System.Drawing.Point(3, 53);
+            this.selectCsvBtn.Name = "selectCsvBtn";
+            this.selectCsvBtn.Size = new System.Drawing.Size(88, 23);
+            this.selectCsvBtn.TabIndex = 3;
+            this.selectCsvBtn.Text = "Select File";
+            this.selectCsvBtn.UseVisualStyleBackColor = true;
+            this.selectCsvBtn.Click += new System.EventHandler(this.selectCsv_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteBtn.Location = new System.Drawing.Point(3, 183);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(88, 23);
+            this.deleteBtn.TabIndex = 4;
+            this.deleteBtn.Text = "Delete ";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // DataGrid
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(837, 266);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "DataGrid";
+            this.Text = "Task";
+            this.Load += new System.EventHandler(this.DataGrid_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button selectCsvBtn;
+        private System.Windows.Forms.Button deleteBtn;
+    }
+}
+
---
+++ DataGrid.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Task
+{
+    public partial class DataGrid : Form
+    {
+        // member variables
+        private static List<string> mDataList = new List<string>();
+        private static SortedDictionary<double, int> mPriceRowDic = new SortedDictionary<double, int>();
+        private static Size size = new Size(0, 0);
+
+        public DataGrid()
+        {
+            try
+            {
+                InitializeComponent();
+                size = this.Size;
+                this.Size = new Size(deleteBtn.Size.Width * 3, this.Height);
+                dataGridView.Hide();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+        }
+
+        private void DataGrid_Load(object sender, EventArgs e)
+        {
+            try
+            {
+               // Do Nothing
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+            
+        }
+
+        private void selectCsv_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SelectFile();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show(@"You are deleting all rows that is not in stock'.\nDo you want to continue?",
+                        "Question", MessageBoxButtons.YesNo)
+                    == DialogResult.Yes)
+                    DeleteNotInRows();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+        }
+
+
+        private void PrepareDataGridView()
+        {
+            try
+            {
+                this.Size = size;
+                this.MinimumSize = size;
+                dataGridView.Show();
+
+                // Data Grid View properties
+                dataGridView.Columns.Clear();
+                dataGridView.Rows.Clear();
+                dataGridView.ReadOnly = false;
+                dataGridView.AllowUserToAddRows = false;
+                dataGridView.AllowUserToDeleteRows = false;
+                dataGridView.AllowUserToResizeRows = false;
+                dataGridView.AutoGenerateColumns = false;
+                dataGridView.AllowUserToResizeColumns = true;
+                dataGridView.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+                dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+                dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+                // add necessary columns
+                dataGridView.Columns.Add(mDataList[0], mDataList[0]);
+                dataGridView.Columns.Add(mDataList[1], mDataList[1]);
+                dataGridView.Columns.Add(mDataList[2], mDataList[2]);
+                dataGridView.Columns.Add(mDataList[3], mDataList[3]);
+
+                DataGridViewCheckBoxColumn inStockCheckBoxColumn =
+                    new DataGridViewCheckBoxColumn
+                    {
+                        Name = mDataList[4],
+                        HeaderText = mDataList[4]
+                    };
+
+                DataGridViewComboBoxColumn bindingBoxColumn = 
+                    new DataGridViewComboBoxColumn
+                    {
+                        Name = mDataList[5], 
+                        HeaderText = mDataList[5], 
+                    };
+
+                DataGridViewButtonColumn descriptionButtonColumn = 
+                    new DataGridViewButtonColumn
+                    {
+                        Name = mDataList[6],
+                        HeaderText = mDataList[6]
+                    };
+
+                dataGridView.Columns.AddRange(new DataGridViewColumn[] {inStockCheckBoxColumn, bindingBoxColumn, descriptionButtonColumn});
+
+                // DGV combo box only opens if it is in edit mode.
+                // If DGVCB clicked then in edit mode else not in edit mode
+                dataGridView.CellMouseClick += (sender, args) =>
+                {
+                    var senderGrid = (DataGridView) sender;
+
+                    senderGrid.ShowCellToolTips = false;
+
+                    if (senderGrid.Columns[args.ColumnIndex] is DataGridViewComboBoxColumn)
+                    {
+                        dataGridView.ReadOnly = false;
+                        dataGridView.EditMode = DataGridViewEditMode.EditOnEnter;
+                    }
+                    else
+                    {
+                        dataGridView.ReadOnly = true;
+                        dataGridView.EditMode = DataGridViewEditMode.EditProgrammatically;
+                    }
+
+                    if (senderGrid.Columns[args.ColumnIndex] is DataGridViewButtonColumn)
+                    {
+                        string desc = senderGrid.Rows[args.RowIndex].Cells[args.ColumnIndex].ToolTipText;
+                        
+                        MessageBox.Show(desc, "Description", MessageBoxButtons.OK);
+                    }
+
+                };
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+        }
+
+        private void SelectFile()
+        {
+            try
+            {
+                // ask user to select csv files
+                OpenFileDialog fileDialog = new OpenFileDialog();
+
+                fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                fileDialog.Filter = "CSV files (*.csv)|*.csv";
+                fileDialog.RestoreDirectory = true;
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    PutData(fileDialog.FileName);
+
+                    NotInStockHighLight();
+
+                    PriceGradient();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+        }
+
+        private void PutData(string fileName)
+        {
+            try
+            {
+                // Read Csv and fill data grid table
+                using (var reader = new StreamReader(fileName))
+                {
+                    bool isFirstLine = true;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        List<string> val = new List<string>(line.Split(';'));
+
+                        if (isFirstLine)
+                        {
+                            mDataList = val;
+                            PrepareDataGridView();
+                            isFirstLine = false;
+                        }
+                        else
+                        {
+                            dataGridView.Rows.Add(val[0], val[1], val[2], val[3], string.Equals(val[4], "yes"), null);
+
+                            int rowsCount = dataGridView.Rows.Count - 1;
+
+                            ((DataGridViewComboBoxCell)dataGridView.Rows[rowsCount].Cells[mDataList[5]]).Items.Add(val[5]);
+                            ((DataGridViewComboBoxCell)dataGridView.Rows[rowsCount].Cells[mDataList[5]]).Value = val[5];
+
+                            ((DataGridViewButtonCell)dataGridView.Rows[rowsCount].Cells[mDataList[6]]).ToolTipText = val[6];
+                            ((DataGridViewButtonCell)dataGridView.Rows[rowsCount].Cells[mDataList[6]]).Value = "...";
+
+                            mPriceRowDic.Add(double.Parse(val[3].Replace(",", ".")), rowsCount);
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+        }
+
+        private void DeleteNotInRows()
+        {
+            try
+            {
+                // delete rows which are not in stock
+                int rowCnt = dataGridView.RowCount;
+                for (int i = 0; i < rowCnt; i++)
+                {
+                    if (!(bool)((DataGridViewCheckBoxCell)dataGridView.Rows[i].Cells[mDataList[4]]).Value)
+                    {
+                        dataGridView.Rows.Remove(dataGridView.Rows[i]);
+                    }
+                }
+
+                PriceGradient();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+        }
+
+        private void NotInStockHighLight()
+        {
+            try
+            {
+                // highlight rows which are not in stock
+                foreach (DataGridViewRow row in dataGridView.Rows)
+                    if (!(bool)((DataGridViewCheckBoxCell)row.Cells[mDataList[4]]).Value)
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Aqua;
+                    }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+
+        }
+
+        private void PriceGradient()
+        {
+            try
+            {
+                // make price cell gradient colors and as much as readable
+                int incrementAmount = 255 / (mPriceRowDic.Count - 1);
+                int counter = 0;
+                foreach (var it in mPriceRowDic)
+                {
+                    int clr = incrementAmount * counter++;
+                    clr = counter == mPriceRowDic.Count ? 255 : clr;
+                    int foreClr = 255 - clr;
+                    dataGridView.Rows[it.Value].Cells[mDataList[3]].Style.BackColor = Color.FromArgb(clr, clr, clr);
+                    dataGridView.Rows[it.Value].Cells[mDataList[3]].Style.ForeColor = Color.FromArgb(60, foreClr, foreClr);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                InformUser(exception.Message);
+            }
+        }
+
+        private void InformUser(string str)
+        {
+            MessageBox.Show(str, "Error", MessageBoxButtons.OK);
+        }
+    }
+}
